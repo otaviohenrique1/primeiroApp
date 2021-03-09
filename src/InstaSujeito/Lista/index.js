65ff444c1e2b7c16e2f228d5af2c76c634818b03
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-03-08 22:45:06 -0300

    werwer

--- src/InstaSujeito/Lista/index.js
+++ src/InstaSujeito/Lista/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+import likePng from "../img/like.png";
+import sendPng from "../img/send.png";
 
 export default class Lista extends Component {
   constructor(props) {
@@ -20,14 +22,33 @@ export default class Lista extends Component {
             style={styles.fotoPerfil}
           />
           <Text style={styles.nomeUsuario}>{nome}</Text>
-          <Image
-            source={{ uri: imgPublicacao }}
-            style={styles.fotoPublicacao}
-          />
-          <Text>{descricao}</Text>
-          <Text>{likeada}</Text>
-          <Text>{likers}</Text>
         </View>
+        <Image
+          resizeMode='cover'
+          source={{ uri: imgPublicacao }}
+          style={styles.fotoPublicacao}
+        />
+        <View style={styles.areaBtn}>
+          <TouchableOpacity>
+            <Image
+              source={likePng}
+              style={styles.iconeLike}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnSend}>
+            <Image
+              source={sendPng}
+              style={styles.iconeLike}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.viewRodape}>
+          <Text style={styles.nomeRodape}>{descricao}</Text>
+          <Text style={styles.descRodape}>{nome}</Text>
+
+        </View>
+        {/* <Text>{likeada}</Text> */}
+        {/* <Text>{likers}</Text> */}
       </View>
     );
   }
@@ -35,8 +56,8 @@ export default class Lista extends Component {
 
 const styles = StyleSheet.create({
   areaFeed: {
-    flex: 1,
-    margin: 10,
+    // flex: 1,
+    // margin: 10,
   },
   nomeUsuario: {
     fontSize: 22,
@@ -48,6 +69,41 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 25,
   },
-  viewPerfil: {},
-  fotoPublicacao: {},
+  fotoPublicacao: {
+    flex: 1,
+    height: 400,
+    alignItems: 'center',
+  },
+  viewPerfil: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    padding: 8,
+  },
+  areaBtn: {
+    flexDirection: 'row',
+    padding: 5,
+  },
+  iconeLike: {
+    width: 33,
+    height: 33,
+  },
+  btnSend: {
+    paddingLeft: 5,
+  },
+  viewRodape: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  descRodape: {
+    paddingLeft: 5,
+    fontSize: 15,
+    color: '#000',
+  },
+  nomeRodape: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000',
+    paddingLeft: 5,
+  },
 });
